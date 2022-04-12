35b6caa34a86ff07bf65cd8c18a816c051ff3c1f
Sam W <sam@sam-we.com>
2022-04-11 18:07:20 -0700

    Rubocopped

--- app/controllers/experiments_controller.rb
+++ app/controllers/experiments_controller.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 # Must rename this
 class ExperimentsController < ApplicationController
   before_action :set_cors_headers
-  before_action :set_experiment, only: %i[ show edit update destroy ]
+  before_action :set_experiment, only: %i[show edit update destroy]
 
   # GET /experiments/1 or /experiments/1.json
   def show
@@ -9,21 +11,22 @@ class ExperimentsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_experiment
-      # @experiment = Experiment.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    # def experiment_params
-    #   params.require(:experiment).permit(:title, :body, :seed)
-    # end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_experiment
+    # @experiment = Experiment.find(params[:id])
+  end
 
-    def set_cors_headers
-      # response.set_header "Access-Control-Allow-Origin", origin
-      headers['Access-Control-Allow-Origin'] = '*'
-      headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-      headers['Access-Control-Request-Method'] = '*'
-      headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    end
+  # Only allow a list of trusted parameters through.
+  # def experiment_params
+  #   params.require(:experiment).permit(:title, :body, :seed)
+  # end
+
+  def set_cors_headers
+    # response.set_header "Access-Control-Allow-Origin", origin
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  end
 end
