0978f84a076b2752fb5780cb0c173df3c26e781f
Sam W <sam@sam-we.com>
2022-04-12 00:09:38 -0700

    app/controllers/products_controller.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,7 +15,7 @@ class ProductsController < ApplicationController
   private
 
   def set_product
-    Rails.logger.info "<< Request for  #{Base64.decode64(params[:displayname]}"
+    Rails.logger.info "<< Request for  #{Base64.decode64(params[:displayname])}"
     @product = Product.find_by_x_displayname! Base64.decode64(params[:displayname])
   end
 
