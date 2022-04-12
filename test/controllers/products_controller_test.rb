10610504afc6cfea98ad6bdea627caa2b028ecda
Sam W <sam@sam-we.com>
2022-04-11 23:11:57 -0700

    test/controllers/products_controller_test.rb

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   test "should show" do
-    get "/products/example"
+    get "/products/U3R1Y2sgSW4gVGhlIENpdHkgU2F0aW4gTWlkaSBEcmVzcyAtIFllbGxvdw=="
     assert_response :success
   end
 end
