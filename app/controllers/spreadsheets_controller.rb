a7b8f8fcb8431d720c45b0797a172ed1beb2e2dd
Sam W <sam@sam-we.com>
2022-04-11 22:43:26 -0700

    app/controllers/spreadsheets_controller.rb

--- app/controllers/spreadsheets_controller.rb
+++ app/controllers/spreadsheets_controller.rb
@@ -11,13 +11,15 @@ class SpreadsheetsController < ApplicationController
 
   def update
     Rails.logger.info "Received #{params[:spreadsheet]}"
+    Product.delete_all
+    
     a = Roo::Spreadsheet.open(params[:spreadsheet])
     # a.header_line = 2
 
-    a.each_with_index(HEADINGS_HASH) do |item, index|
-      next if index == 0
-      puts "> #{item.inspect}"
-    end
+    Product.insert_all(a.each_with_index(HEADINGS_HASH))
+    #  next if index == 0
+    #  puts "> #{item.inspect}"
+    #end
     flash[:notice] = "Uploaded"
     redirect_to "/upload"
   end
