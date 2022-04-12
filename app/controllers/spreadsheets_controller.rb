47b747cfcd612dbe370e018678a36136e3f272d5
Sam W <sam@sam-we.com>
2022-04-11 21:52:38 -0700

    app/controllers/spreadsheets_controller.rb

--- app/controllers/spreadsheets_controller.rb
+++ app/controllers/spreadsheets_controller.rb
@@ -7,8 +7,9 @@ class SpreadsheetsController < ApplicationController
 
   def update
     Rails.logger.info "Received #{params[:spreadsheet]}"
-    workbook = Roo::Spreadsheet.open params[:spreadsheet]
-    workbook.first_sheet..each_row_streaming(offset: 3) do |row|
+    Roo::Spreadsheet.open(params[:spreadsheet])
+      .sheet('Sheet1')
+      .each_row_streaming(offset: 3) do |row|
       next if row[0].is_a?(Roo::Excelx::Cell::Empty)
       puts row[0]
     end
