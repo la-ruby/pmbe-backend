fc47a8d541860f477766847cdbb5f7a6b0e0b83e
Sam W <sam@sam-we.com>
2022-04-11 21:21:07 -0700

    config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   get '/products/:display_name', to: 'products#show'
+  resource :spreadsheet, only: [:edit, :update]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
