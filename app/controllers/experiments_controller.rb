aa4cc4df1517f2f727724a6b80794b71a2d0e456
Sam W <sam@sam-we.com>
2022-04-11 18:01:17 -0700

    Relax cors

--- app/controllers/experiments_controller.rb
+++ app/controllers/experiments_controller.rb
@@ -1,4 +1,5 @@
 class ExperimentsController < ApplicationController
+  before_action :set_cors_headers
   before_action :set_experiment, only: %i[ show edit update destroy ]
 
   # GET /experiments/1 or /experiments/1.json
@@ -15,5 +16,13 @@ class ExperimentsController < ApplicationController
     # Only allow a list of trusted parameters through.
     def experiment_params
       params.require(:experiment).permit(:title, :body, :seed)
+    end
+
+    def set_cors_headers
+      # response.set_header "Access-Control-Allow-Origin", origin
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+      headers['Access-Control-Request-Method'] = '*'
+      headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     end
 end
